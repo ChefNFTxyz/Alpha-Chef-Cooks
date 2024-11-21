7ed800b837980be844e8ecacf1b7636ee087ac53
ChefNFTxyz <188594208+ChefNFTxyz@users.noreply.github.com>
2024-11-21 15:40:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ allowlist.tsx
@@ -0,0 +1,21 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
+//You can also add them here and copy the route from the console log 
+export const allowLists = new Map<string, Array<string>> ([
+    ["Head",
+    [
+      "HestHarPSdXG2sKGsogkyhLeo9Tddof1k4iSKmAXDTHG",
+      "CV8DqUvYDWMByMN3P2SDi7ErajuhsDUo8gEfyVDZBRtJ"
+    ]],
+    ["Public",
+      [
+        "6GVi2zyUDLYj1fPotMemwfzExevxMBofNEBYnKPhqj7h",
+        "Dbe9FwPWHPeYfpAECjeJtNdAWS5XsPgJR2AwgSG556WE", 
+        "9uprGfLKaJLTViRfCcNRenTCBGWQEQ7UUMvA4VD9wid3",
+        "5oLvKj8HDRLdwYsFq96wMKiYcGhmfDZSSSCQf1HNR7j8",
+        "AcYdwzjef2dPsF1Nw6k7a1Gh24PxXZK3uLEPMBB4ou4R"
+    ]],
+    ["All",
+      [
+        "HestHarPSdXG2sKGsogkyhLeo9Tddof1k4iSKmAXDTHG"
+    ]],
+]);
---
+++ settings.tsx
@@ -0,0 +1,14 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "Head", mintText: "", buttonLabel: "Mint now!", header: "" },
+  { label: "Public", mintText: "", buttonLabel: "Mint now!", header: "" },
+  { label: "All", mintText: "", buttonLabel: "Mint now!", header: "" },
+  { label: "default", mintText: "", buttonLabel: "Mint now!", header: ""}
+];
+
+//header image in the ui. replace with your own
+export const image = "https://i.imgur.com/S4TE6RE.png";
+
+//website title
+export const headerText = "";
